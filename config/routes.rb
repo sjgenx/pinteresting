43469ccc355e7bdf386f523e38dcfc1dff072b4e
Shaun Jacobsen <sjgenx@gmail.com>
2014-07-17 15:05:07 -0600

    Created Home and About Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Pinteresting::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about" #creates about_path
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+ 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
